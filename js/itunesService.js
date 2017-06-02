9b78c372903e2b87949293de86594ef4b51a6583
Brobbiethao <Brobbiethao@gmail.com>
2017-06-01 23:45:51 -0500

    Gothelp

--- js/itunesService.js
+++ js/itunesService.js
@@ -3,30 +3,45 @@ angular.module('itunes').service('itunesService', function($http, $q){
   //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
-  //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //https://itunes.apple.com/search?term=' + artist + '&callback=JSONP_CALLBACK'
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
 
+  this.getArtist = function(artist) {
 
+    var deferred = $q.defer();
 
+    $http.jsonp('https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK')
+      .then(function(response) {
+        var data = response.data.results;
 
-    // Go to the next step in the README (Tie in your controller). You will come back to these instructions shortly.
-    // 
-    // You need to sort the data you get back from the itunes API to be an object in the following format.
-    /*
-      AlbumArt: "http://a3.mzstatic.com/us/r30/Features4/v4/22/be/30/22be305b-d988-4525-453c-7203af1dc5a3/dj.srlprmuo.100x100-75.jpg"
-      Artist: "Nelly"
-      Song: "Ride Wit Me"
-      Collection: "Nellyville"
-      CollectionPrice: 11.99
-      Play: "http://a423.phobos.apple.com/us/r1000/013/Music4/v4/4a/ab/7c/4aab7ce2-9a72-aa07-ac6b-2011b86b0042/mzaf_6553745548541009508.plus.aac.p.m4a"
-      Type: "song"
-  */
-  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this finalized array of data. 
-  // When this is complete, head back to your controller.
+        deferred.resolve(data);
+      });
 
+    return deferred.promise;
+
+  };
+
+  this.formatArtistData = function (result) {
+    var parsedResults = [];
+
+    for(var i = 0; i < result.length; i++) {
+      var newObj = {
+        AlbumArt : result[i].artworkUrl100,
+        Artist : result[i].artistName,
+        Collection : result[i].collectionCensoredName,
+        CollectionPrice : result[i].collectionPrice,
+        Play : result[i].previewUrl,
+        Type : result[i].kind
+      };
+
+      parsedResults.push(newObj);
+    }
+
+    return parsedResults;
+
+  };
 
 });
